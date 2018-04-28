1a2b5b8ab69138c9af37c3f020d1726353896185
Maciek <bojek.maciej@gmail.com>
2018-04-28 19:51:07 +0200

    Added constructor and setDepartment method

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -7,4 +9,26 @@ public class Email {
     private String department;
     private int mailboxCapacity;
     private String alternateEmail;
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = setDepartment();
+    }
+
+    private String setDepartment() {
+        System.out.print("Select department:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter code:");
+        Scanner sc = new Scanner(System.in);
+        int selectedCode = sc.nextInt();
+
+        if(selectedCode == 1) {
+            return "sales";
+        } else if(selectedCode == 2) {
+            return "development";
+        } else if(selectedCode == 3) {
+            return "accounting";
+        } else {
+            return "";
+        }
+    }
 }
