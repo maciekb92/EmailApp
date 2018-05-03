e625c2ddc5f019323349f78699ee0baca45edf69
Maciek <bojek.maciej@gmail.com>
2018-05-03 19:52:16 +0200

    Added email generator

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,13 +7,22 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
+    private String email;
     private int mailboxCapacity;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
+    private String companySuffix = "company.com";
 
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        System.out.println("First name: " + this.firstName + ", Last name: " + this.lastName);
         this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Password: " + this.password);
+        email = this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@" + this.department.toLowerCase() + "." + companySuffix;
+        System.out.println("Email: " + email);
     }
 
     private String setDepartment() {
@@ -30,5 +39,15 @@ public class Email {
         } else {
             return "";
         }
+    }
+
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for(int i = 0; i < length; i++) {
+            int rand = (int)(Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
     }
 }
